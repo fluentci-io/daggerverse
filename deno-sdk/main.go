f20577199e493ba7a127e85f3c8cdabb7c99f3bd
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-11-23 06:43:41 +0000

    use specific runtime version

--- deno-sdk/main.go
+++ deno-sdk/main.go
@@ -30,7 +30,7 @@ func (m *DenoSdk) ModuleRuntime(modSource *Directory, subPath string, introspect
 			"install",
 			"--reload",
 			"-A",
-			"https://cdn.jsdelivr.net/gh/fluentci-io/daggerverse@6596302/deno-sdk/sdk/src/mod/cli.ts",
+			"https://cdn.jsdelivr.net/gh/fluentci-io/daggerverse@50250bd/deno-sdk/sdk/src/mod/cli.ts",
 			"-n",
 			"runtime",
 		}).
